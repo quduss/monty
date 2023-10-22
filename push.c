5c200387935765a663c25a445ec5fc4b6118d14a
qudys <shuaibqudus33@gmail.com>
2023-10-21 19:13:26 -0700

    Fix betty errors

--- push.c
+++ push.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 /**
  * push_ - pushes a new node to the stack
- * @head_node: pointer to pointer to the head node 
+ * @head_node: pointer to pointer to the head node
  * @line_number: line number
  */
 void push_(stack_t **head_node, unsigned int line_number)
@@ -17,7 +17,7 @@ void push_(stack_t **head_node, unsigned int line_number)
 	}
 
 	for (k = 0; glob_var.token_2[k] != '\0'; k++)
-        {
+	{
 		if (glob_var.token_2[k] != '-' && !isdigit(glob_var.token_2[k]))
 		{
 			dprintf(2, "L%u: ", line_number);
